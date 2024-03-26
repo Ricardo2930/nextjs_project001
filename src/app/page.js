88e5339ac62b3325569f9cc13171229d4463b0b3
Ricardo2930 <rhuaytan@gmail.com>
2024-03-26 14:49:42 -0500

    style: :art: format main page

--- src/app/page.js
+++ src/app/page.js
@@ -6,17 +6,16 @@ import Nosotros from "@/components/Nosotros";
 import Servicios from "@/components/Servicios";
 import Testimonios from "@/components/Testimonios";
 
-
 export default function Home() {
   return (
     <main className="">
-      <Navbar/>
-      <Main/>
-      <Servicios/>
-      <Nosotros/>
-      <Experiencia/>
-      <Testimonios/>
-      <Footer/>
+      <Navbar />
+      <Main />
+      <Servicios />
+      <Nosotros />
+      <Experiencia />
+      <Testimonios />
+      <Footer />
     </main>
   );
 }
