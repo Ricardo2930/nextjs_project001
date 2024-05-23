252289b11501d8e06f1fe7ef89e79510ee9c2c5a
Ricardo2930 <rhuaytan@gmail.com>
2024-05-23 11:20:01 -0500

    style: :art: Se actualizo los estilos en el componente Servicios.jsx

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -1,64 +1,64 @@
 export default function Servicios() {
   return (
     <section
-      id="servicios"
-      className="text-center bg-gray-900 py-12 sm:my-20 sm:rounded-tl-full sm:rounded-bl-full sm:shadow-2xl sm:shadow-gray-900/50"
-    >
-      <h2 className="text-4xl sm:text-6xl font-bold text-gray-100">
-        Que hacemos
-      </h2>
-      <h3 className="text-4xl sm:text-6xl text-gray-400 border-b-8 border-yellow-300 inline-block">
-        para ayudarte
-      </h3>
-      <div className="grid justify-between p-5 items-center gap-20 sm:flex sm:p-14">
-        <div className="bg-gray-200 rounded-xl p-5">
-          <h4 className="sm:text-2xl font-semibold border-b-4 border-yellow-300 inline-block">
-            Servicio 001
-          </h4>
-          <p className="text-pretty py-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-            repellendus provident, asperiores omnis, suscipit vel accusantium
-            fuga autem maiores, corporis deserunt mollitia? Et iure excepturi
-            aliquam dolor minima minus accusamus.
-          </p>
-        </div>
-
-        <div className="bg-gray-200 rounded-xl p-5">
-          <h4 className="sm:text-2xl font-semibold border-b-4 border-yellow-300 inline-block">
-            Servicio 002
-          </h4>
-          <p className="text-pretty py-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-            repellendus provident, asperiores omnis, suscipit vel accusantium
-            fuga autem maiores, corporis deserunt mollitia? Et iure excepturi
-            aliquam dolor minima minus accusamus.
-          </p>
-        </div>
-
-        <div className="bg-gray-200 rounded-xl p-5">
-          <h4 className="sm:text-2xl font-semibold border-b-4 border-yellow-300 inline-block">
-            Servicio 003
-          </h4>
-          <p className="text-pretty py-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-            repellendus provident, asperiores omnis, suscipit vel accusantium
-            fuga autem maiores, corporis deserunt mollitia? Et iure excepturi
-            aliquam dolor minima minus accusamus.
-          </p>
-        </div>
-
-        <div className="bg-gray-200 rounded-xl p-5">
-          <h4 className="sm:text-2xl font-semibold border-b-4 border-yellow-300 inline-block">
-            Servicio 004
-          </h4>
-          <p className="text-pretty py-5">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-            repellendus provident, asperiores omnis, suscipit vel accusantium
-            fuga autem maiores, corporis deserunt mollitia? Et iure excepturi
-            aliquam dolor minima minus accusamus.
-          </p>
-        </div>
+    id="servicios"
+    className="text-center bg-gray-900 py-12 sm:my-20 lg:rounded-l-full sm:shadow-2xl sm:shadow-gray-900/50"
+  >
+    <h2 className="text-5xl md:text-6xl 2xl:text-7xl font-bold text-gray-100">
+      Que hacemos
+    </h2>
+    <h3 className="text-4xl md:text-5xl 2xl:text-6xl text-gray-400 border-b-8 border-yellow-300 inline-block">
+      para ayudarte
+    </h3>
+    <div className="grid gap-5 md:grid md:grid-cols-2 2xl:grid-cols-4 justify-between p-5 items-center md:gap-5 lg:gap-10 2xl:gap-20 sm:p-12">
+      <div className="bg-gray-200 rounded-xl p-5">
+        <h4 className="sm:text-2xl font-semibold border-b-4 border-yellow-300 inline-block">
+          Servicio 001
+        </h4>
+        <p className="text-pretty py-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+          repellendus provident, asperiores omnis, suscipit vel accusantium
+          fuga autem maiores, corporis deserunt mollitia? Et iure excepturi
+          aliquam dolor minima minus accusamus.
+        </p>
       </div>
-    </section>
+
+      <div className="bg-gray-200 rounded-xl p-5">
+        <h4 className="sm:text-2xl font-semibold border-b-4 border-yellow-300 inline-block">
+          Servicio 002
+        </h4>
+        <p className="text-pretty py-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+          repellendus provident, asperiores omnis, suscipit vel accusantium
+          fuga autem maiores, corporis deserunt mollitia? Et iure excepturi
+          aliquam dolor minima minus accusamus.
+        </p>
+      </div>
+
+      <div className="bg-gray-200 rounded-xl p-5">
+        <h4 className="sm:text-2xl font-semibold border-b-4 border-yellow-300 inline-block">
+          Servicio 003
+        </h4>
+        <p className="text-pretty py-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+          repellendus provident, asperiores omnis, suscipit vel accusantium
+          fuga autem maiores, corporis deserunt mollitia? Et iure excepturi
+          aliquam dolor minima minus accusamus.
+        </p>
+      </div>
+
+      <div className="bg-gray-200 rounded-xl p-5">
+        <h4 className="sm:text-2xl font-semibold border-b-4 border-yellow-300 inline-block">
+          Servicio 004
+        </h4>
+        <p className="text-pretty py-5">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+          repellendus provident, asperiores omnis, suscipit vel accusantium
+          fuga autem maiores, corporis deserunt mollitia? Et iure excepturi
+          aliquam dolor minima minus accusamus.
+        </p>
+      </div>
+    </div>
+  </section>
   );
 }
